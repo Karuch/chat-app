508c950115bf3052a63e5743ed28691e590473a0
Karuch <talk474747@gmail.com>
2024-01-14 18:47:22 +0200

    adding all files + db intergration works

---
+++ src/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+  "database/sql"
+  "fmt"
+  "log"
+  _ "github.com/lib/pq"
+)
+
+const (
+  host     = "172.17.0.2"
+  port     = 5432
+  user     = "postgres"
+  password = "1598"
+  dbname   = "postgres"
+)
+
+func main() {
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+  db, err := sql.Open("postgres", psqlInfo)
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+
+  err = db.Ping()
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("Successfully connected!")
+
+  var dbName string
+  err = db.QueryRow("SELECT current_database()").Scan(&dbName)
+  if err != nil {
+	  log.Fatal(err)
+  }
+  fmt.Println(dbName)
+}
