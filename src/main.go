f27a00e84dfa3c6673db68a673ee0675012c001e
Karuch <talk159820@gmail.com>
2024-01-17 10:46:26 +0200

    seems that go able to connect redis and set values

--- src/main.go
+++ src/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
   //"main/postgres"
+  "context"
   "fmt"
+  "github.com/redis/go-redis/v9"
 )
 
 func main() {
   fmt.Println("test")
+  client := redis.NewClient(&redis.Options{
+      Addr:	  "172.17.0.2:6379",
+      Password: "1598", // no password set
+      DB:		  0,  // use default DB
+  })
+
+  ctx := context.Background()
+
+  err := client.Set(ctx, "foo", "bar", 0).Err()
+  if err != nil {
+      panic(err)
+  }
+
+  val, err := client.Get(ctx, "foo").Result()
+  if err != nil {
+      panic(err)
+  }
+  fmt.Println("foo", val)
 }
 
