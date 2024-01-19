0e68d29dc2bd88c67c7a0d89df2001b1b861e006
Karuch <talk159820@gmail.com>
2024-01-19 16:01:23 +0200

    I think I done with redis part Delete Set Getall Get

--- src/main.go
+++ src/main.go
@@ -11,9 +11,10 @@ import (
 func main() {
   common.ENVinit()
   fmt.Println(common.Convert_to_int(os.Getenv("POSTGRES_PORT")))
-  redis.Set(redis.Ctx, redis.Connect_to_db(), "tal", "hello redis!")
-
-  redis.Getall(redis.Ctx, redis.Connect_to_db(), "tal")
-  //redis.Get(redis.Ctx, redis.Connect_to_db(), "c3224db8-22d7-4fcc-bb4f-3f1cfa53a732")
+  fmt.Println(redis.Delete(redis.Ctx, redis.Connect_to_db(), "5491590d-e568-44d0-8c7d-c6785ebf7e2a"))
+  fmt.Println(redis.Set(redis.Ctx, redis.Connect_to_db(), "tal", "hello redis"))
+  fmt.Println(redis.Getall(redis.Ctx, redis.Connect_to_db(), "tal"))
+  fmt.Println(redis.Get(redis.Ctx, redis.Connect_to_db(), "cdbe806a-9357-47cf-a6bf-a423611eb710"))
+  
 }
 
--- src/redis/redis.go
+++ src/redis/redis.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"fmt"
 	"main/common"
-	"github.com/redis/go-redis/v9"
 	"os"
+	"github.com/redis/go-redis/v9"
 )
 
 /* examples
-Set(ctx, connect_to_db(), "elad", uuid.New().String(), "testiiii")
-Delete(ctx, connect_to_db(), "elad", "0")
-Getall(ctx, connect_to_db(), "elad")
-Amount(ctx, connect_to_db(), "elad") */
+  fmt.Println(redis.Delete(redis.Ctx, redis.Connect_to_db(), "5491590d-e568-44d0-8c7d-c6785ebf7e2a"))
+  fmt.Println(redis.Set(redis.Ctx, redis.Connect_to_db(), "tal", "hello redis"))
+  fmt.Println(redis.Getall(redis.Ctx, redis.Connect_to_db(), "tal"))
+  fmt.Println(redis.Get(redis.Ctx, redis.Connect_to_db(), "cdbe806a-9357-47cf-a6bf-a423611eb710")) */
 
 
 
@@ -27,82 +27,84 @@ func Connect_to_db() *redis.Client { //wonder if that's a good idea it means tha
   return client
 }
 
-func Set(ctx context.Context, client *redis.Client, user string, message string){
+func Set(ctx context.Context, client *redis.Client, user string, message string) string {
+	// Specify the key and fields
+	var id string = common.Random_uuid()
 	fields := map[string]interface{}{
 		"date":    common.Current_date_for_message(),
 		"user":  user,
 		"message": message,
 	}
 
-	// Use the HSet command to set the values
-	result, err := client.HSet(ctx, common.Random_uuid(), fields).Result()
+	// Use the HSet method to set the values
+	_, err := client.HSet(ctx, id, fields).Result()
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return "error"
 	}
 
 	// Print the result (1 if a new field was created, 0 if field already existed and was updated)
-	fmt.Println("HSET Result:", result)
-}
-
-func Getall(ctx context.Context, client *redis.Client, user string){
-	iter := client.Scan(ctx, 0, "*", 0).Iterator()
-	for iter.Next(ctx) {
-		result, err := client.HGet(ctx, iter.Val(), "user").Result()
-		if err != nil {
-			fmt.Println("Error:", err)
-		}
-		if result == user {
-			Get(ctx, client, iter.Val())
-		}
-	}
-	if err := iter.Err(); err != nil {
-		panic(err)
-	}
-}
-
-
-func Delete(ctx context.Context, client *redis.Client, key string){
-	cmd := client.HDel(ctx, key)
-	if err := cmd.Err(); err != nil {
-		panic(err)
-	}
-	if cmd.Val() <= 0 {
-		fmt.Println("the id", key, "was not found")
-	} else {
-		fmt.Println("hdelete was successful.")
-	}
-	println("Fields deleted: ", cmd.Val())
 	
+	return fmt.Sprintf("add message: %v ] %v ] %v : %v\n", id, fields["date"], fields["user"], fields["message"])
 }
 
 
-func Amount(ctx context.Context, client *redis.Client, user string) int {
-	var count int = 0
+func Getall(ctx context.Context, client *redis.Client, user string) []string {
+	all_messages := []string{}
 	iter := client.Scan(ctx, 0, "*", 0).Iterator()
 	for iter.Next(ctx) {
 		result, err := client.HGet(ctx, iter.Val(), "user").Result()
+		
 		if err != nil {
 			fmt.Println("Error:", err)
 		}
 		if result == user {
-			count++
+			all_messages = append(all_messages, Get(ctx, client, iter.Val()))
 		}
 	}
 	if err := iter.Err(); err != nil {
 		panic(err)
 	}
-	return count
+	return all_messages
 }
 
 
-func Get(ctx context.Context, client *redis.Client, key string) {
-	result, err := client.HGet(ctx, key, "*").Result()
+func Delete(ctx context.Context, client *redis.Client, key string) string {
+	exists_key_check, err_key_check := client.Exists(ctx, key).Result()
+	if err_key_check != nil {
+		fmt.Println("Error checking key existence:", err_key_check)
+		return "error"
+	}
+	if exists_key_check != 1 {
+		return fmt.Sprintf("message ID '%s' does not exist.\n", key)
+	}
+	//actual function v
+
+	_, err := client.Del(ctx, key).Result()
+	if err != nil {
+		return fmt.Sprintf("%s", err)
+	}
+	return fmt.Sprintf("message ID '%s' has been deleted successfully.", key)
+
+}
+
+func Get(ctx context.Context, client *redis.Client, key string) string {
+	exists_key_check, err_key_check := client.Exists(ctx, key).Result()
+	if err_key_check != nil {
+		fmt.Println("Error checking key existence:", err_key_check)
+		return "error"
+	}
+	if exists_key_check != 1 {
+		return fmt.Sprintf("message ID '%s' does not exist.\n", key)
+	}
+	//actual function v
+	
+	fields := []string{"date", "user", "message"}
+	values, err := client.HMGet(ctx, key, fields...).Result()
 	if err != nil {
 		fmt.Println("Error:", err)
-		
 	}
-	fmt.Println(result)
+	return fmt.Sprintf(" %v ] %v ] %v : %v", key, values[0], values[1], values[2])
 }
 
 
