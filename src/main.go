f5d748b5fb9b37da0ba74d4a819c0a7ee30cf053
Karuch <talk159820@gmail.com>
2024-01-22 23:35:47 +0200

    create_user from client to server seems to be working however client can choose whatever user he wants and all code is a mess

--- src/main.go
+++ src/main.go
@@ -2,61 +2,89 @@ package main
 
 import (
 	//"encoding/json"
-	"fmt"
-	"main/common"
-	"main/jwtHandler"
-	"time"
-
-	"github.com/golang-jwt/jwt"
 	"main/auth"
+	"main/common"
+	"main/postgres"
+
 	//"main/postgres"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+
 
 
 
 func main() {
 	common.ENVinit()
-	
-	// user login validation should occur here
+	// Create a new Gin router
+	router := gin.Default()
 
-	userClaims := jwtHandler.UserClaims{
-		Username: "Leeroy",
-		StandardClaims: jwt.StandardClaims{
-		IssuedAt:  time.Now().Unix(),
-		ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
-		},
-	}
-	
-	refreshClaims := jwt.StandardClaims{
-		IssuedAt:  time.Now().Unix(),
-		ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
-	}
-	
-	access := jwtHandler.New_signed_access_token(userClaims)
-	refresh := jwtHandler.New_signed_refresh_token(refreshClaims)
-	fmt.Println(access)
-	fmt.Println(refresh)
-	ref := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDYwMDQwMTksImlhdCI6MTcwNTgzMTIxOX0.unBaz6XPaPRHw4CH7FnG6D8uijT2fG-Rx4dc1sbg2i8"
-	parsrefresh := jwtHandler.ParseRefreshToken(ref)
-	
-	
-	issuedAtTime := time.Unix(parsrefresh.IssuedAt, 0)
-	expiresAtTime := time.Unix(parsrefresh.ExpiresAt, 0)
-	notBeforeTime := time.Unix(parsrefresh.NotBefore, 0)
+	// Create a route group for "/user"
+	userGroup := router.Group("/user")
 
-	
-	fmt.Println(issuedAtTime)
-	fmt.Println(expiresAtTime)
-	fmt.Println(notBeforeTime)
-	
-	//currentTime := time.Now()
-	//secondsDifference := expiresAtTime.Sub(currentTime).Seconds()
-	auth.Check_half_life_refresh_need_new(ref)
+	// Define the "/user/create" endpoint
+	userGroup.POST("/create", createUserHandler)
 
-	
+	// Define the "/user/login" endpoint
+	userGroup.POST("/login", loginUserHandler)
+
+	// Run the server on port 8080
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}	
 }	
 
 
+type Client_login struct {
+	User string `json:"user"`
+	Password string `json:"password"`
+}
+
+
+type Client_response struct {
+	Body string `json:"Body"`
+	Authorization string `json:"Authorization"`
+}
+
+
+func response_handler(c *gin.Context, ) Client_login {
+	ResponsejsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		// Handle error
+	}
+	
+	var client_login Client_login
+
+	err = json.Unmarshal(ResponsejsonData, &client_login)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	
+	return client_login
+}
+
+
+
+func createUserHandler(c *gin.Context) { //user/create
+	// Handle response
+	c.JSON(http.StatusOK, gin.H{
+		"status": "Request sent successfully",
+		"token": "token",
+	})
+	fmt.Println(auth.Create_user(postgres.Client_connect(), response_handler(c).User, response_handler(c).Password))
+	fmt.Println(postgres.Get_all_messages(postgres.Client_connect(), "guyz"))
+}
+
+func loginUserHandler(c *gin.Context) {
+	// Handle the "/user/login" endpoint logic
+	c.JSON(http.StatusOK, gin.H{"message": "User logged in"})
+}
 
 
 
@@ -66,4 +94,42 @@ func main() {
 	if r := recover(); r != nil {
 		common.CustomErrLog.Println("Recovered from PANIC", r)
 	}
-}()*/
+}()
+
+
+
+
+/*r.GET("/ping", func(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+	fmt.Println("here", c.GetHeader("Authorization"))
+})
+
+
+r.POST("/user", func(c *gin.Context) {
+
+	// Handle response
+	c.JSON(http.StatusOK, gin.H{
+		"status": "Request sent successfully",
+		"token": "token",
+	})
+
+	jsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		// Handle error
+	}
+	
+
+	var client_response Client_response
+
+	err = json.Unmarshal(jsonData, &client_response)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	client_response.Authorization = c.GetHeader("Authorization")
+
+	logger.Println("Body:", client_response.Body)
+	logger.Println("Auth:", client_response.Authorization)
+})*/
