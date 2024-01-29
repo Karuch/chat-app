f246f96146799ee35e66333acc4f1a7dce570ed8
Karuch <talk159820@gmail.com>
2024-01-29 11:59:00 +0200

    seperate all endpoints to server folder

--- src/main.go
+++ src/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	"main/server"
+	"fmt"
+	"main/auth"
+	//"main/server"
+	"main/jwtHandler"
+	"main/common"
 )
 
 
@@ -9,5 +13,23 @@ import (
 
 
 func main(){
-	server.Start()
+	common.ENVinit()
+	//auth.Check_half_life_refresh_need_new("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY2ODI4ODUsImlhdCI6MTcwNjUxMDA4NSwic3ViIjoibGlvciJ9.XqJ2Kh33mCHvHgBiVtXYTbkrCyFSLIaSqRdPz3bVyEU")
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY2OTQ2NTksImlhdCI6MTcwNjUyMTg1OSwic3ViIjoibGlvciJ9.aB2NvDE8QK0HjLkLjyEGp3QP1p2FV4wEGUrRO3wjhtI"
+	
+	parsedToken, err := jwtHandler.ParseRefreshToken(token)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(parsedToken.Subject)
+
+
+
+	newToken, err := jwtHandler.NewRefreshToken(auth.Refresh_claim_creator(parsedToken.Subject))
+	if err != nil { //if happen there's a problem with access claim creator or newaccesstoken the above checker should be valid if it got to here
+		fmt.Println(err)
+	}
+
+	fmt.Println(newToken)
+	//server.Start()
 }
--- src/server/server.go
+++ src/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"main/common"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,7 +13,6 @@ import (
 
 
 func Start() {
-	common.ENVinit()
 	// Create a new Gin router
 	router := gin.Default()
 
